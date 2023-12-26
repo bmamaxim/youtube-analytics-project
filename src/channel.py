a210eab8fbf384a21be6d71cfaf87dae421cac91
bmamaxim <bma@gmail.com>
2023-12-26 23:43:28 +0500

    service

--- src/channel.py
+++ src/channel.py
@@ -1,6 +1,12 @@
 import os
 
+from googleapiclient.discovery import build
+
 api_key: str = os.getenv('API_KEY')
+
+def get_service():
+    return build('youtube', 'v3', developerKey=api_key)
+
 
 class Channel:
     """Класс для ютуб-канала"""
@@ -8,7 +14,8 @@ class Channel:
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
+        self.channel = get_service().channels().list(id=channel_id, part='snippet,statistics').execute()
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        print(self.channel_id)
+        print(self.channel)
