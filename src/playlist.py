668cd5d9daa152fa1a43b9d29ed05e4439aed798
bmamaxim <bma@gmail.com>
2024-01-05 15:58:42 +0500

    hw5

--- src/playlist.py
+++ src/playlist.py
@@ -10,18 +10,19 @@ from src.video import Video
 class PlayList:
     api_key: str = os.getenv('API_KEY')
 
-    def __init__(self, channel_id: str):
-        self.channel_id = channel_id
-        #self.playlists = self.get_service().playlists().list(channelId=channel_id,
-                                                             #part='contentDetails,snippet',
-                                                             #maxResults=50, ).execute()
-        # self.title = self.playlists['items'][0]['snippet']['title']
-        # self.playlist_videos = self.get_service().playlistItems().list(playlistId=self._id,
-        # part='contentDetails',
-        # maxResults=50).execute()
-        # self.video_ids: list[str] = [video['contentDetails']['videoId'] for video in self.playlist_videos['items']]
-        # self.videos = [Video(video_id) for video_id in self.video_ids]
-        # self.url = self.get_service()
+    def __init__(self, _id: str):
+        self.playlist_id = _id
+        self.playlist = self.get_service().playlists().list(id=self.playlist_id,
+                                                            part='snippet',
+                                                            maxResults=50).execute()
+        self.title = self.playlist.get('items')[0]['snippet']['title']
+        self.info_videos = self.get_service().playlistItems().list(playlistId=self.playlist_id,
+                                                                   part='contentDetails, snippet',
+                                                                   maxResults=50).execute()
+        self.video_ids = [video['contentDetails']['videoId'] for video in self.info_videos['items']]
+        self.videos = [Video(video_id) for video_id in self.video_ids]
+        self.url = f"https://www.youtube.com/playlist?list={self.playlist_id}"
+
 
     @classmethod
     def get_service(cls):
@@ -41,21 +42,13 @@ class PlayList:
             playlist_duration += isodate.parse_duration(video.duration)
         return playlist_duration
 
+
     def show_best_video(self):
         """
         Возвращает ссылку на самое популярное видео из плейлиста (по количеству лайков)
         """
-        return max(self.videos, key=lambda i: i.likes_count)
+        best_video: Video = max(self.videos, key=lambda i: i.like_count)
+        return best_video.url
 
-
-channel_id = 'PLv_zOGKKxVpj-n2qLkEM2Hj96LO6uqgQw'
-api_key: str = os.getenv('API_KEY')
-youtube = build('youtube', 'v3', developerKey=api_key)
-
-playlists = youtube.playlists().list(channelId=channel_id,
-                                     part='contentDetails,snippet',
-                                     maxResults=50,
-                                     ).execute()
-print(api_key)
-print(youtube.playlists().list(channel_id))
-#print(playlists)
+    def __repr__(self):
+        return f"{self.url}"
